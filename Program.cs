a24b495d757bc7e630df54374c7ae8d50cc29266
aina gbolahan sunday <aina gbolahan sunday@DESKTOP-167JB72>
2022-09-09 09:23:46 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARRAY
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.ReadLine();
+
+            
+            
+            int[] n = { 7, 2, 6, 15, 54, 10, 23 };
+            int inputNo1 = n[7];
+
+            int total = 0;
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (n[i] < 0);
+                total = n[i];
+
+                
+            }
+            return total;
+
+
+
+
+        }
+    }
+}
